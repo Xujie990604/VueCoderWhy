f4375f4000017952f03b450631704147161752f4
x09898 <coder_xujie@163.com>
2022-06-16 19:28:13 +0800

    2022-06-16

---
+++ src/views/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="home-page">
+       bb
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomePage',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/TestView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="test-view">
+        test
+       <el-button type="primary" icon="el-icon-check">按钮</el-button>
+       <el-input> </el-input>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TestView',
+    data() {
+        return {
+
+        } 
+    }
+}
+</script>
+
+<style scoped>
+button {
+    color: red;
+}
+
+.el-input >>>  .el-input__inner {
+    height: 100px;
+}
+</style>
