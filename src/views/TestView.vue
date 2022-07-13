c8f2175c845e38245616d040e0f2965aced07809
x09898 <coder_xujie@163.com>
2022-07-13 17:58:04 +0800

    2022-07-13

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -2,16 +2,14 @@
  * @Author: x09898 coder_xujie@163.com
  * @Date: 2022-06-16 17:27:06
  * @LastEditors: x09898 coder_xujie@163.com
- * @LastEditTime: 2022-07-11 13:35:28
+ * @LastEditTime: 2022-07-13 14:43:32
  * @FilePath: \VueCoderWhy\src\views\TestView.vue
  * @Description: 专门用来测试的页面
 -->
 
 <template>
   <div class="test-view">
-    <ul>
-      <li v-for="(item,index) of arr" :key="index">{{ item }}</li>
-    </ul>
+    {{ testName }}
   </div>
 </template>
 <script>
@@ -19,20 +17,19 @@ export default {
   name: 'TestView',
   data() {
     return {
-      name: 'app',
-      isTrue: true,
-      arr: ['1', '2', '3']
     }
   },
-  beforeCreate() {
-  },
-  created() {
-    window.data = this
-  },
-  computed: {
+  props: {
+    testName: {
+      type: String,
+      default: 'test-name'
+    }
   }
 }
 </script>
 
 <style scoped>
+.test-view {
+  color: red;
+}
 </style>
--- src/views/homePage/HomePage.vue
+++ src/views/homePage/HomePage.vue
@@ -1,13 +1,24 @@
 <!--
  * @Author: xujie 1607526161@qq.com
  * @Date: 2022-07-10 14:24:59
- * @LastEditors: xujie 1607526161@qq.com
- * @LastEditTime: 2022-07-10 14:25:05
- * @FilePath: \supermarket\src\views\homePage\HomePage.vue
+ * @LastEditors: x09898 coder_xujie@163.com
+ * @LastEditTime: 2022-07-12 13:53:27
+ * @FilePath: \VueCoderWhy\src\views\homePage\HomePage.vue
  * @Description: home视图组件
 -->
 <template>
-    <div class="home-page">
-        home页面
-    </div>
+  <div class="home-page">
+    home页面
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data() {
+    return {
+      lists: ['a', 'd']
+    }
+  }
+}
+</script>
