d94c02acddca53871f4ac2ae4cdfa89f7e9f25b5
x09898 <coder_xujie@163.com>
2022-07-15 17:57:00 +0800

    2022-07-15

--- src/views/category/CateGory.vue
+++ src/views/category/CateGory.vue
@@ -1,13 +1,23 @@
 <!--
  * @Author: xujie 1607526161@qq.com
  * @Date: 2022-07-10 14:26:50
- * @LastEditors: xujie 1607526161@qq.com
- * @LastEditTime: 2022-07-10 14:26:57
- * @FilePath: \supermarket\src\views\category\CateGory.vue
+ * @LastEditors: x09898 coder_xujie@163.com
+ * @LastEditTime: 2022-07-15 17:56:29
+ * @FilePath: \VueCoderWhy\src\views\category\CateGory.vue
  * @Description: 分类视图组件
 -->
 <template>
-    <div class="cate-gory">
-        分类页面
-    </div>
+  <div class="cate-gory">
+    分类页面
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'CateGory',
+  data() {
+    return {
+    }
+  },
+}
+</script>
