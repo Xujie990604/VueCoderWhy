77ce28a94ef888c54eef00e848de91a51a81d1ba
xujie <1607526161@qq.com>
2022-07-17 16:53:49 +0800

    2022-07-17

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -2,23 +2,21 @@
  * @Author: x09898 coder_xujie@163.com
  * @Date: 2022-06-16 17:27:06
  * @LastEditors: xujie 1607526161@qq.com
- * @LastEditTime: 2022-07-14 21:01:01
+ * @LastEditTime: 2022-07-17 16:49:46
  * @FilePath: \supermarket\src\views\TestView.vue
  * @Description: 专门用来测试的页面
 -->
 
 <template>
-  <div class="test-view">
-  </div>
+  <div class="test-view"></div>
 </template>
 <script>
 export default {
-  name: 'TestView',
+  name: "TestView",
   data() {
-    return {
-    }
-  }
-}
+    return {};
+  },
+};
 </script>
 
 <style scoped>
--- src/views/homePage/HomePage.vue
+++ src/views/homePage/HomePage.vue
@@ -2,18 +2,20 @@
  * @Author: xujie 1607526161@qq.com
  * @Date: 2022-07-10 14:24:59
  * @LastEditors: xujie 1607526161@qq.com
- * @LastEditTime: 2022-07-14 21:00:03
+ * @LastEditTime: 2022-07-17 16:51:37
  * @FilePath: \supermarket\src\views\homePage\HomePage.vue
  * @Description: home视图组件
 -->
 <template>
   <div class="home-page">
-    home页面
+    <nav-bar></nav-bar>
   </div>
 </template>
 
 <script>
+import NavBar from '@/components/common/navBar/NavBar.vue'
 export default {
+  components: { NavBar },
   name: 'HomePage',
   data() {
     return {
