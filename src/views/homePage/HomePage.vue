d356ef81f433c7c2058872f1cccde82adc15be16
xujie <1607526161@qq.com>
2022-07-23 13:01:33 +0800

    1

--- src/views/homePage/HomePage.vue
+++ src/views/homePage/HomePage.vue
@@ -2,7 +2,7 @@
  * @Author: xujie 1607526161@qq.com
  * @Date: 2022-07-10 14:24:59
  * @LastEditors: xujie 1607526161@qq.com
- * @LastEditTime: 2022-07-23 12:54:15
+ * @LastEditTime: 2022-07-23 13:01:21
  * @FilePath: \supermarket\src\views\homePage\HomePage.vue
  * @Description: home视图组件
 -->
